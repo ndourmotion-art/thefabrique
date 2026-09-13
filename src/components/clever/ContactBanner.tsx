d78bf2b74d73b937a7aa7b6e80d6005bb18889b7
ndourmotion-art <ndourmotion@gmail.com>
2026-09-13 13:15:17 +0200

    fix all image paths to public/media

--- src/components/clever/ContactBanner.tsx
+++ src/components/clever/ContactBanner.tsx
@@ -1,11 +1,9 @@
-import banniereAsset from "@/assets/banniere.jpg.asset.json";
-
 export const ContactBanner = () => {
   return (
     <section className="py-8 md:py-12 bg-background">
       <div className="mx-auto max-w-[1600px] px-6 lg:px-10">
         <img
-          src={banniereAsset.url}
+          src="/media/banniere.jpg"
           alt="The FABRIQUE - Be visible, to exist!"
           className="w-full h-auto rounded-2xl"
           loading="lazy"
